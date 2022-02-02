0943a159fca49bcb0fdc11c9b07b1f787ca90fb1
Alan Andrew Lopez Meneses <brm15541@BRM15541H.local>
2022-02-01 18:48:23 -0600

    Se borran archivos ajenos a productos y categoria

--- src/main/java/com/example/capacitacion/repositories/CuentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.capacitacion.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.capacitacion.models.Cuenta;
-
-public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
-
-}
